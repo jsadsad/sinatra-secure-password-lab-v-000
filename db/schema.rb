b42c7965cf59a5e046eb6521c7f284b0ba308809
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-05-09 23:50:49 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20190509232750) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
